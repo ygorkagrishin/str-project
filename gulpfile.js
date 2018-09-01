a892444d18c627f081b790de3f67fb7bc95e5404
ygorkagrishin <webygorkagrishin@gmail.com>
2018-09-01 16:11:41 +0300

    Добавлен конфигурационный файл в котором прописаны пути к файлам

--- gulpfile.js
+++ gulpfile.js
@@ -53,196 +53,158 @@ const rename = require('gulp-rename');
 // чистим папку
 const del = require('del');
 
-// Пути к основным файлам
-const paths = {
-    pug: {
-        src: 'assets/pug',
-        dest: 'public/'
-    },
-    stylus: {
-        src: 'assets/static/styles',
-        dest: 'public/'
-    },
-    scripts: {
-        src: 'assets/static/js',
-        dest: 'public/'
-    },
-    fonts: {
-        src: 'assets/static/fonts',
-        dest: 'public/fonts/'
-    },
-    images: {
-        src: 'assets/static/img',
-        dest: 'public/img/'
-    },
-    icons: {
-        src: 'assets/static/svg',
-        dest: 'public/'
-    },
-    baseDir: 'public'
-}
+// Подключаем конфигурационный файл
+const conf = require('./settings.json');
 
-// Пути к остальным файлам
-const otherFiles = {
-    addCssBefore: [
-        'node_modules/normalize.css/normalize.css'
-    ],
-    addJsBefore: [
-        'node_modules/jquery/dist/jquery.min.js'
-    ] 
-}
-
-const cssCacheName = 'csscache';
-const jsCacheName = 'jscache';
-
-const options = Object.assign(paths, otherFiles);
+// Подключаем пути
+const paths = conf.paths;
+const files = conf.files;
 
 // Определение: разработка это или финальная сборка
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 // Чистим папку
 gulp.task('del', () => {
-    return del(options.baseDir + '/*');
+  return del(paths.baseDir + '/*');
 });
 
 // Собираем разметку
 gulp.task('html:build', () => {
-    return gulp.src(options.pug.src + '/*.pug')
-    .pipe(plumber({
-        errorHandler: err => {
-            notify.onError({
-                title: 'html build error',
-                message: err.message
-            })(err)
-        }
-    }))
-    .pipe(debug({title: 'html'}))
-    .pipe(pug(gulpif(isDevelopment, {pretty: true })))
-    .pipe(gulp.dest(options.pug.dest));
+  return gulp.src(paths.pug.src + '/*.pug')
+  .pipe(plumber({
+    errorHandler: err => {
+      notify.onError({
+        title: 'html build error',
+        message: err.message
+      })(err)
+    }
+  }))
+  .pipe(debug({title: 'html'}))
+  .pipe(pug(gulpif(isDevelopment, {pretty: true })))
+  .pipe(gulp.dest(paths.pug.dest));
 });
 
 // Копируем файлы css
 gulp.task('css:copy', () => {
-    return gulp.src(options.addCssBefore)
-        .pipe(gulp.dest(options.baseDir + '/'))
+  return gulp.src(files.css)
+  .pipe(gulp.dest(paths.baseDir + '/'));
 });
 
 // Собираем стили
 gulp.task('css:build', () => {
-    return gulp.src(options.stylus.src + '/styles.styl')
-        .pipe(plumber({
-            errorHandler: err => {
-                notify.onError({
-                    title: 'css build error',
-                    message: err.message
-                })(err)
-            }
-        }))
-        .pipe(cached(cssCacheName))
-        .pipe(debug({title: 'css'}))
-        .pipe(gulpif(isDevelopment, sourcemaps.init()))
-        .pipe(remember(cssCacheName))
-        .pipe(stylus(gulpif(!isDevelopment, {compress: true, 'include css': true})))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(rename('styles.min.css'))
-        .pipe(gulpif(isDevelopment, sourcemaps.write('.')))
-        .pipe(gulp.dest(options.stylus.dest));
+  return gulp.src(paths.stylus.src + '/styles.styl')
+  .pipe(plumber({
+    errorHandler: err => {
+      notify.onError({
+        title: 'css build error',
+        message: err.message
+      })(err)
+    }
+  }))
+  .pipe(cached('csscache'))
+  .pipe(debug({title: 'css'}))
+  .pipe(gulpif(isDevelopment, sourcemaps.init()))
+  .pipe(remember('csscache'))
+  .pipe(stylus(gulpif(!isDevelopment, {compress: true, 'include css': true})))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+  .pipe(rename('styles.min.css'))
+  .pipe(gulpif(isDevelopment, sourcemaps.write('.')))
+  .pipe(gulp.dest(paths.stylus.dest));
 });
 
 // Копируем файлы скриптов
 gulp.task('js:copy', () => {
-    return gulp.src(options.addJsBefore)
-        .pipe(gulp.dest(options.baseDir + '/'));
+  return gulp.src(files.js)
+  .pipe(gulp.dest(paths.baseDir + '/'));
 });
 
 // Собираем скрипты
 gulp.task('js:build', () => {
-    return gulp.src(options.scripts.src + '/*.js')
-        .pipe(plumber({
-            errorHandler: err => {
-                notify.onError({
-                    title: 'js build error',
-                    message: err.message
-                })(err)
-            }
-        }))
-        .pipe(cached(jsCacheName))
-        .pipe(debug({title: 'js'}))
-        .pipe(gulpif(isDevelopment, sourcemaps.init()))
-        .pipe(remember(jsCacheName))
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulpif(!isDevelopment, uglify()))
-        .pipe(concat('common.min.js'))
-        .pipe(gulpif(isDevelopment, sourcemaps.write('.')))
-        .pipe(gulp.dest(options.scripts.dest));
+  return gulp.src(paths.scripts.src + '/*.js')
+  .pipe(plumber({
+    errorHandler: err => {
+      notify.onError({
+        title: 'js build error',
+        message: err.message
+      })(err)
+    }
+  }))
+  .pipe(cached('jscache'))
+  .pipe(debug({title: 'js'}))
+  .pipe(gulpif(isDevelopment, sourcemaps.init()))
+  .pipe(remember('jscache'))
+  .pipe(babel({
+    presets: ['env']
+  }))
+  .pipe(gulpif(!isDevelopment, uglify()))
+  .pipe(concat('common.min.js'))
+  .pipe(gulpif(isDevelopment, sourcemaps.write('.')))
+  .pipe(gulp.dest(paths.scripts.dest));
 });
 
 // Копируем шрифты
 gulp.task('fonts:copy', () => {
-    return gulp.src(options.fonts.src + '/**/*.{ttf,woff,woff2,eot,svg}')
-        .pipe(newer(options.fonts.dest))
-        .pipe(gulp.dest(options.fonts.dest));
+  return gulp.src(paths.fonts.src + '/**/*.{ttf,woff,woff2,eot,svg}')
+  .pipe(newer(paths.fonts.dest))
+  .pipe(gulp.dest(paths.fonts.dest));
 });
 
 // Копируем картинки
 gulp.task('img:copy', () => {
-    return gulp.src(options.images.src + '/**/**/*.{png,jpg}')
-        .pipe(newer(options.images.dest))
-        .pipe(imagemin({
-            optimizationLevel: 5
-        }))
-        .pipe(gulp.dest(options.images.dest));
+  return gulp.src(paths.images.src + '/**/**/*.{png,jpg}')
+  .pipe(newer(paths.images.dest))
+  .pipe(imagemin({
+    optimizationLevel: 5
+  }))
+  .pipe(gulp.dest(paths.images.dest));
 });
 
 // Собирааем svg
 gulp.task('svg:sprite', () => {
-    return gulp.src(options.icons.src + '/*.svg')
-    .pipe(newer(options.icons.dest))
-    .pipe(svgmin(function (file) {
-        return {
-            plugins: [{
-                cleanupIDs: {
-                minify: true
-                }   
-            }]
-        }
-    }))
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(cheerio({
-        run: function($) {
-            $('svg').attr('style',  'display:none');
-        },
-        parserOptions: {
-            xmlMode: true
-        }
-    }))
-    .pipe(rename('sprite-svg.svg'))
-    .pipe(gulp.dest(options.icons.dest))
+  return gulp.src(paths.svg.src + '/*.svg')
+  .pipe(newer(paths.svg.dest))
+  .pipe(svgmin(function (file) {
+    return {
+      plugins: [{
+        cleanupIDs: {
+          minify: true
+        }   
+      }]
+    }
+  }))
+  .pipe(svgstore({inlineSvg: true}))
+  .pipe(cheerio({
+    run: function($) {
+      $('svg').attr('style', 'display:none');
+    },
+    parserOptions: {
+      xmlMode: true
+    }
+  }))
+  .pipe(rename('sprite-svg.svg'))
+  .pipe(gulp.dest(paths.svg.dest));
 });
 
 gulp.task('watch', () => {
-    gulp.watch(options.pug.src + '/**/*.pug', gulp.series('html:build'))
-    gulp.watch(options.stylus.src + '/**/*.styl', gulp.series('css:build'))
-    gulp.watch(options.scripts.src + '/*.js', gulp.series('js:build'))
-    gulp.watch(options.fonts.src + '/**/*.{ttf,woff,woff2,eot,svg}', gulp.series('fonts:copy'))
-    gulp.watch(options.images.src + '/**/**/*.{png,jpg}', gulp.series('img:copy'))
-    gulp.watch(options.icons.src + '/**/*.svg', gulp.series('svg:sprite'))
+  gulp.watch(paths.pug.src + '/**/*.pug', gulp.series('html:build'));
+  gulp.watch(paths.stylus.src + '/**/*.styl', gulp.series('css:build'));
+  gulp.watch(paths.scripts.src + '/*.js', gulp.series('js:build'));
+  gulp.watch(paths.fonts.src + '/**/*.{ttf,woff,woff2,eot,svg}', gulp.series('fonts:copy'));
+  gulp.watch(paths.images.src + '/**/**/*.{png,jpg}', gulp.series('img:copy'));
+  gulp.watch(paths.svg.src + '/**/*.svg', gulp.series('svg:sprite'));
 });
 
 gulp.task('serve', () => {
-    browserSync.init({
-        server: options.baseDir + '/'
-    });
-
-    gulp.watch(options.baseDir + '/**/**/*.*').on('change', browserSync.reload);    
+  browserSync.init({
+    server: paths.baseDir + '/'
+  });
+  gulp.watch(paths.baseDir + '/**/**/*.*').on('change', browserSync.reload);    
 });
 
 gulp.task('build', 
-    gulp.series('del', 'fonts:copy', 'img:copy', 'svg:sprite', 'css:copy', 'js:copy', 'html:build', 'css:build', 'js:build'));
+  gulp.series('del', 'fonts:copy', 'img:copy', 'svg:sprite', 'css:copy', 'js:copy', 'html:build', 'css:build', 'js:build'));
 
 // Собираем проект
 gulp.task('default', gulp.series('build', gulp.parallel('watch', 'serve')));
